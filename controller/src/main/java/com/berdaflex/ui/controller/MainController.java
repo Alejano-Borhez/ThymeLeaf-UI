45b2e00acfcf0b0c676f553ec3589ac54225d6ab
Alejano-Borhez <alexander.borohov17@gmail.com>
2016-10-03 19:01:06 +0300

    Project clean-up

--- controller/src/main/java/com/berdaflex/ui/controller/MainController.java
+++ controller/src/main/java/com/berdaflex/ui/controller/MainController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+
     @RequestMapping("/hello")
     public String hello() {
         return "hello";
     }
+
 }
